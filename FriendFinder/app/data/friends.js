838eea385d31847e8c2c41cce162727a2cb934aa
Ragland <jakearagland@gmail.com>
2018-04-18 15:52:25 -0500

    just started almost done with home page css just need to footer on bot

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,70 @@
+
+// still need to change the photo
+var friendsArray = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            '5',
+            '1',
+            '4',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1'
+        ]
+    },
+    {
+        name: "Sam",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            '3',
+            '2',
+            '1',
+            '4',
+            '2',
+            '1',
+            '2',
+            '2',
+            '4',
+            '1'
+        ]
+    },
+    {
+        name: "John",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            '1',
+            '1',
+            '5',
+            '4',
+            '2',
+            '3',
+            '2',
+            '5',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Sarah",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            '1',
+            '5',
+            '3',
+            '3',
+            '4',
+            '1',
+            '2',
+            '2',
+            '4',
+            '3'
+        ]
+    },
+
+];
+
